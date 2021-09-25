cda48377d64e68d8835849d44e90f30e2f1192c4
David Ström <david.strom88@gmail.com>
2021-09-25 11:16:45 +0200

    Add input to change name

--- src/views/DetailsView.js
+++ src/views/DetailsView.js
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { UserContext } from "../provider/UserProvider";
 
 const DetailsView = () => {
   const history = useHistory();
   const [username, setUsername] = useContext(UserContext);
+  const [name, setName] = useState();
 
   useEffect(() => {});
 
@@ -12,7 +13,9 @@ const DetailsView = () => {
     <div>
       <h1>Details View!</h1>
       <h2>Your name is {username}</h2>
-      <button onClick={() => setUsername("David")}>Change Name to David</button>
+      <h3>Change name into {name}?</h3>
+      <input onChange={(event) => setName(event.target.value)} />
+      <button onClick={() => setUsername(name)}>Change Name</button>
       <button onClick={() => history.push("/")}>Go Home</button>
     </div>
   );
